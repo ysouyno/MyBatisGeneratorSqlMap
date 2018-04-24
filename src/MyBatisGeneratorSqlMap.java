08b8c78b2c877e2194a73ca2f2218f2e460b6fbf
ysouyno <ysouyno@163.com>
2018-04-24 23:52:02 +0800

    add java code and xml configuration file

---
+++ src/MyBatisGeneratorSqlMap.java
@@ -0,0 +1,33 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+public class MyBatisGeneratorSqlMap {
+	public void generator() throws Exception {
+		List<String> warnings = new ArrayList<String>();
+		boolean overwrite = true;
+		File configFile = new File("GeneratorConfig.xml");
+		ConfigurationParser cp = new ConfigurationParser(warnings);
+		Configuration config = cp.parseConfiguration(configFile);
+		DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+		MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config,
+				callback, warnings);
+		myBatisGenerator.generate(null);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MyBatisGeneratorSqlMap myBatisGeneratorSqlMap = new MyBatisGeneratorSqlMap();
+		try {
+			myBatisGeneratorSqlMap.generator();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
